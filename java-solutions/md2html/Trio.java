fdd4138f06f55011d5a5c503de0aa8a6f6360acc
javaddikkt <167349259+javaddikkt@users.noreply.github.com>
2024-04-17 21:17:49 +0300

    md2html package

---
+++ java-solutions/md2html/Trio.java
@@ -0,0 +1,12 @@
+package md2html;
+
+public class Trio {
+    public String string;
+    public int integer;
+    public boolean bool;
+    public Trio (String string, int integer, boolean bool) {
+        this.string = string;
+        this.integer = integer;
+        this.bool = bool;
+    }
+}
